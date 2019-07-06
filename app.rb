2e736e95a3b47d749365b816609e0549d30d1d65
Donna Amos <donnacamos.developer@gmail.com>
2019-07-06 04:11:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
       params[:pirate][:ships].each do |details|
         Ship.new(details) 
       end 
-      @ships = params[:ship]
+      @ships = Ship.all
       erb :display 
     end
   end
