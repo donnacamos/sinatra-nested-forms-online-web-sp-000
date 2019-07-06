788f2f5fedb21f1c77576c7bb0d5702d156272f5
Donna Amos <donnacamos.developer@gmail.com>
2019-07-06 03:28:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    # define your routes here
+    get '/' do
+      erb :root
+    end
 
-    get '/pirate' do 
-      :
+    get '/new' do
+      erb :form
+    end
 
+    post '/pirates' do
+      @pirate = params[:pirate]
+      @ships = params[:ship]
+      erb :display 
+    end
   end
 end
